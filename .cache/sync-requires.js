37243dda2bc48bfdd2a26da14bde1785bdf13ca9
Steve Sultan <59545545+SteveSultan@users.noreply.github.com>
2022-01-05 16:49:26 -0500

    commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/sultanforwork/Downloads/personalItem/my-new-site-1/.cache/dev-404-page.js")),
+  "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": preferDefault(require("/Users/sultanforwork/Downloads/personalItem/my-new-site-1/node_modules/@wkocjan/gatsby-theme-intro/src/pages/404.js")),
+  "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js": preferDefault(require("/Users/sultanforwork/Downloads/personalItem/my-new-site-1/node_modules/@wkocjan/gatsby-theme-intro/src/templates/index.js"))
+}
+
